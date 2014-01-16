a906829279db064b47e2f07931b2b0fddb17ce5f
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-01-16 16:21:30 +0100

    Updated license headers.

--- system-off-wakeup-on-gpio/main.c
+++ system-off-wakeup-on-gpio/main.c
@@ -1,15 +1,8 @@
-/* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
+/* Copyright (c) 2013 Nordic Semiconductor. All Rights Reserved.
  *
- * The information contained herein is confidential property of Nordic
- * Semiconductor ASA.Terms and conditions of usage are described in detail
- * in NORDIC SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the license.txt file.
  */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf.h"
